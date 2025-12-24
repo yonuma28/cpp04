bef32635dea138c3a163567f658076169fc94298
yonuma28 <kisaragi.12056@gmail.com>
2025-12-24 15:18:01 +0900

    add: ex03 の hpp 追加

---
+++ ex03/IMateriaSource.hpp
@@ -0,0 +1,18 @@
+#ifndef IMATERIASOURCE_HPP
+# define IMATERIASOURCE_HPP
+
+# include <string>
+# include <iostream>
+
+# include "ICharacter.hpp"
+# include "Amateria.hpp"
+
+class IMateriaSource
+{
+    public:
+        virtual ~IMateriaSource() {}
+        virtual void learnMateria(AMateria*) = 0;
+        virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
+#endif //IMATERIASOURCE_HPP
